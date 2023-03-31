ac0cf81d93ef038aee5dd605ae7052ec6688a779
Ivan Rybalko <ivribalko@gmail.com>
2023-03-30 22:24:27 -0400

    store and load key

--- lib/ioc.dart
+++ lib/ioc.dart
@@ -1,3 +1,5 @@
+import 'package:dart_openai/openai.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
 
@@ -7,7 +9,10 @@ import 'ui/common.dart';
 
 class IoC {
   static Future init() async {
+    await dotenv.load();
     await GetStorage.init(kSave);
+
+    OpenAI.apiKey = dotenv.env['OPEN_AI_API_KEY']!;
 
     GetStorage(kSave).listenKey(
       kDarkMode,
