f7081211a3c05077f94cec7fbab59c032c28ee95
Ivan Rybalko <ivribalko@gmail.com>
2023-04-03 01:12:07 -0400

    don't focus

--- lib/ui/home/home_page.dart
+++ lib/ui/home/home_page.dart
@@ -16,34 +16,36 @@ class HomePage extends StatelessWidget {
       child: Scaffold(
         body: Stack(
           children: [
-            Obx(
-              () => ListView(
-                padding: const EdgeInsets.all(kPadding),
-                physics: const ClampingScrollPhysics(),
-                children: list
-                    .map((e) => Column(children: [
-                          Stack(
-                            children: [
+            Focus(
+              descendantsAreFocusable: false,
+              canRequestFocus: false,
+              child: Obx(
+                () => ListView(
+                  padding: const EdgeInsets.all(kPadding),
+                  physics: const ClampingScrollPhysics(),
+                  children: list
+                      .map((e) => Column(children: [
+                            Stack(children: [
                               ChadMarkdown(e),
                               Positioned(
-                                top: -5,
-                                right: -10,
-                                child: GoogleButton(e.input))
-                          ]),
-                          const Divider(),
-                        ]))
-                    .cast<Widget>()
-                    .toList()
-                  ..add(const Opacity(
-                    opacity: 0,
-                    child: TextField(maxLines: ChadInput.kMaxLines),
-                  )),
+                                  top: -5,
+                                  right: -10,
+                                  child: GoogleButton(e.input))
+                            ]),
+                            const Divider(),
+                          ]))
+                      .cast<Widget>()
+                      .toList()
+                    ..add(const Opacity(
+                      opacity: 0,
+                      child: TextField(maxLines: ChadInput.kMaxLines),
+                    )),
+                ),
               ),
             ),
-            Column(
-              children: [
-                const Spacer(),
-                ChadInput(),
+            Column(children: [
+              const Spacer(),
+              ChadInput(),
             ])
           ],
         ),
